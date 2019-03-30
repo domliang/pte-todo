8fc6df71a5a0eb0d802803a5f2628b1b63fb7a80
dom <dom@boo.chat>
2019-03-30 22:53:12 +0800

    refactor(store): add comment for store action

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -2,11 +2,12 @@ import { shortId } from '../utils/id';
 import { getTodosFromLocal } from './localStore';
 
 export default {
+  // 初始化todos
   async init({ commit }: {commit: any}) {
     const todos = await getTodosFromLocal();
     commit('init', todos);
   },
-
+  // 添加新todo
   addTodo({ commit }: {commit: any}, text: string) {
     commit('addTodo', {
       id: shortId(),
@@ -14,25 +15,25 @@ export default {
       done: false,
     });
   },
-
+  // 删除 todo
   removeTodo({ commit }: {commit: any}, todo: any) {
     commit('removeTodo', todo);
   },
-
+  // toggle todo 状态
   toggleTodo({ commit }: {commit: any}, todo: any) {
     commit('editTodo', { todo, done: !todo.done });
   },
-
+  // 编辑todo
   editTodo({ commit }: {commit: any}, { todo, value }: { todo: any, value: string }) {
     commit('editTodo', { todo, text: value });
   },
-
+  // toggle所有todo
   toggleAll({ state, commit }: {state: any, commit: any}, done: any) {
     state.todos.forEach((todo: any) => {
       commit('editTodo', { todo, done: !todo.done });
     });
   },
-
+  // 清除全部已完成
   clearCompleted({ state, commit }: {state: any, commit: any}) {
     state.todos.filter((todo: any) => todo.done)
       .forEach((todo: any) => {
