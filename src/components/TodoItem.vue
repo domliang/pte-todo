0a6eb5c6db3fb37d6cdb936dbbda7baa79781403
dom <dom@boo.chat>
2019-03-30 16:28:54 +0800

    feat(itemui): change item ui

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,23 +1,30 @@
 <template>
     <li class="todo-item">
-      <div class="view" v-show="!editing">
+      <div class="view">
         <!-- toggles for todo -->
-        <input class="toggle"
+        <!-- <input class="toggle"
           type="checkbox"
           :checked="todo.done"
-          @change="toggleTodo(todo)">
+          @change="toggleTodo(todo)"> -->
+          <!-- el-icon-check -->
+        <i class="el-icon-circle-check toggle" v-show="todo.done" @click="toggleTodo(todo)"></i>
+        <i class="el-icon-circle-check-outline toggle" v-show="!todo.done" @click="toggleTodo(todo)"></i>
+        <!-- 编辑按钮 -->
+        <i class="el-icon-more edit" @click="editing = true"></i>
+        <!-- <el-button type="primary" class="edit" icon="el-icon-more" circle @click="editing = true"></el-button> -->
         <!-- todo文字显示区域 -->
-        <div class="thetext" v-text="todo.text" @dblclick="editing = true"></div>
+        <div class="thetext" v-text="todo.text"></div>
         <!-- 任务删除按钮 -->
-        <el-button  class="destroy" type="danger" icon="el-icon-delete" circle @click="removeTodo(todo)"></el-button>
+        <i class="el-icon-close destroy" @click="removeTodo(todo)"></i>
       </div>
-      <!-- 编辑状态下输入框 -->
-      <input class="edit"
-        v-show="editing"
-        :value="todo.text"
-        @keyup.enter="doneEdit"
-        @keyup.esc="cancelEdit"
-        @blur="doneEdit">
+
+      <el-dialog title="Edit Todo" :visible.sync="editing">
+        <el-input placeholder="Please input" v-model="todo.text"></el-input>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="cancelEdit()">Cancel</el-button>
+          <el-button type="primary" @click="doneEdit()">Confirm</el-button>
+        </span>
+      </el-dialog>      
     </li>
   <!-- </div> -->
 </template>
@@ -25,7 +32,6 @@
 <style scoped lang="less">
 .todo-item {
   border-radius: 4px;
-  height: 50px;
   line-height: 50px;
   border-bottom: dashed gray;
 }
@@ -34,20 +40,30 @@
   height: 50px;
 }
 .destroy {
+  font-size: 26px;
+  color: red;
   position: absolute;
-  top: 0;
+  top: 12px;
   right: 0;
-  bottom: 0;
-  width: 40px;
-  height: 40px;
-  margin: auto 0;
-  border: none;
+}
+.toggle {
+  font-size: 26px;
+  color: dodgerblue;
+  position: absolute;
+  left: 0;
+  top: 12px;
+}
+.edit {
+  font-size: 26px;
+  position: absolute;
+  left: 40px;
+  top: 12px;
 }
 .thetext {
   position: absolute;
   top: 0;
   right: 40px;
-  left: 40px;
+  left: 80px;
   bottom: 0;
 }
 </style>
@@ -87,9 +103,9 @@ export default class TodoItem extends Vue {
   // actions end
 
   // 编辑todo
-  private doneEdit(e: any): void {
-    const value = e.target.value.trim();
+  private doneEdit(): void {
     const { todo } = this;
+    const value = todo.text;
     if (!value) {
       this.removeTodo(todo);
     } else if (this.editing) {
@@ -102,8 +118,7 @@ export default class TodoItem extends Vue {
   }
 
   // 取消编辑状态
-  private cancelEdit(e: any): void {
-    e.target.value = this.todo.text;
+  private cancelEdit(): void {
     this.editing = false;
   }
 }
