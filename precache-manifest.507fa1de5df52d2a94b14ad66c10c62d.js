6279f222fb1fe2c46376183fcecf9dc29733cf54
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-30 00:59:27 +0000

    Deploy domliang/pte-todo to github.com/domliang/pte-todo.git:gh-pages

---
+++ precache-manifest.507fa1de5df52d2a94b14ad66c10c62d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f06e1abfddab9297c1c3",
+    "url": "/js/chunk-vendors.8681114a.js"
+  },
+  {
+    "revision": "393d2c36928517bafe7c",
+    "url": "/js/app.b04eee01.js"
+  },
+  {
+    "revision": "d7c849ad540c6c2a0455",
+    "url": "/js/about.ad763791.js"
+  },
+  {
+    "revision": "4944ff0f30d89940eb989726359b6a77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "393d2c36928517bafe7c",
+    "url": "/css/app.254c6164.css"
+  }
+];
