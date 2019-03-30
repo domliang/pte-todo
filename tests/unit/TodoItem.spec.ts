c72205d938ee8e1f7691b51b1f652ac5691a0df9
dom <dom@boo.chat>
2019-03-30 23:37:47 +0800

    test(test): Adding missing tests e2e and unit

--- tests/unit/TodoItem.spec.ts
+++ tests/unit/TodoItem.spec.ts
@@ -1,17 +1,31 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/TodoItem.vue';
+import TodoItem from '@/components/TodoItem.vue';
 
 describe('TodoItem.vue', () => {
-  it('renders props.msg when passed', () => {
+  it('TodoItem active item', () => {
     const todo = {
       id: 'id1',
       text: 'text1',
       done: false,
     };
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(TodoItem, {
       propsData: { todo },
       stubs: ['el-dialog', 'el-input', 'el-button'],
     });
     expect(wrapper.find('.thetext').text()).toMatch(todo.text);
+    expect(wrapper.find('.el-icon-circle-check').element.style.display).toMatch('none');
+  });
+  it('TodoItem done item', () => {
+    const todo = {
+      id: 'id2',
+      text: 'text2',
+      done: true,
+    };
+    const wrapper = shallowMount(TodoItem, {
+      propsData: { todo },
+      stubs: ['el-dialog', 'el-input', 'el-button'],
+    });
+    expect(wrapper.find('.thetext').text()).toMatch(todo.text);
+    expect(wrapper.find('.el-icon-circle-check-outline').element.style.display).toMatch('none');
   });
 });
